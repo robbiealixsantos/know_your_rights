c5f6bbe46300e1359d644d04144562aea4ef212e
Robbie Santos <robbiesantos@Robbies-MacBook-Pro.local>
2017-02-19 13:44:54 -0800

    Remove decks resource

--- app/models/deck.rb
+++
@@ -1,4 +0,0 @@
-class Deck < ActiveRecord::Base
-  has_many :questions
-  has_many :answers, :through => :questions
-end
--- db/migrate/20170219211348_create_decks.rb
+++
@@ -1,9 +0,0 @@
-class CreateDecks < ActiveRecord::Migration[5.0]
-  def change
-    create_table :decks do |t|
-      t.string   :category
-
-      t.timestamps
-    end
-  end
-end
