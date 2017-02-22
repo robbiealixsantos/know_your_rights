51b5ab1b266c1e729a166eea328ae7403e57e558
Robbie Santos <robbiesantos@Robbies-MacBook-Pro.local>
2017-02-21 22:04:19 -0800

    Add radio form for answers and javascript for radio toggle function

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    radioButtonToggle();
+    answerSubmitAction();
+});
+
+var radioButtonToggle = function(){
+    $('input[name="response"]').click(function(){
+        var $radio = $(this);
+        // if this was previously checked
+        if ($radio.data('waschecked') == true)
+        {
+            $radio.prop('checked', false);
+            $radio.data('waschecked', false);
+        }
+        else
+            $radio.data('waschecked', true);
+        // remove was checked from other radios
+        $radio.siblings('input[name="response"]').data('waschecked', false);
+        });
+};
+
+var answerSubmitAction = function(){
+
+};
+
+
--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -4,7 +4,5 @@ class QuizzesController < ApplicationController
     @question = questions.sample
   end
 
-  def show
 
-  end
 end
