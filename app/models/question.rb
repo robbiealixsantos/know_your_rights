bf35eaaf010e1a6b5c1c0eee1f319a51135b906a
tereg <teregalla@gmail.com>
2017-03-03 13:58:53 -0800

    Rename quizzes view and controller to questions view and controller, update check_answer method in question model, and update create method in questions controller

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,15 @@
 class Question < ApplicationRecord
   has_many :answers, :dependent => :delete_all
 
-  def self.check_answer(response)
-    if response == "true"
-      puts "correct"
+  def check_answer(response)
+    answers = self.answers
+    correct_answer = answers.find { |answer| answer.correct == true }
+    
+    if response == correct_answer.answer
+      "That is correct!"
+    else
+      "The correct answer is '#{correct_answer.answer}'"
     end
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :quizzes
   resources :questions
 
-  root 'quizzes#index'
+  root 'questions#index'
 end
